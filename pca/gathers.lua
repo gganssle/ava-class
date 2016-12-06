18bb7b1d0084fbd28fbba21d1ee42cb0faa6d59b
gganssle <gr@m-gan.sl>
2016-12-06 21:00:44 +0000

    PCA on all gathers

--- pca/gathers.lua
+++ pca/gathers.lua
@@ -1,8 +1,9 @@
---[[ 'bbimgath' the data has 1501 time samples and 45 traces per gather]]--
+--[[ 'bbimgath' the data has 1501 time samples and varying traces per gather]]--
 
 require 'unsup'
 
 ns = 1501
+--DEV: FIX GATHER TERMINATOR---------------------------------------
 ng = 45
 ntr = 7101405
 
@@ -14,27 +15,41 @@ os.remove('/home/ubuntu/bbimgath/eigenvectors')
 outfile = torch.DiskFile('/home/ubuntu/bbimgath/eigenvectors', 'w')
 outfile:binary()
 
+os.remove('/home/ubuntu/bbimgath/parfile')
+
 -- initialize
 dat = torch.Tensor(ns,ng)
-counter = 10
+counter = 1
+timer = torch.Timer()
 
 for k = 1, ntr, ng do
 	-- progress
 	if k/ntr > counter then
+		parfile = io.open('/home/ubuntu/bbimgath/parfile', 'a')
 		print('percentage complete: ', k/ntr, '%\n')
-		counter = counter + 10
+		parfile:write('percentage complete: ', k/ntr, '%\n')
+		counter = counter + 1
+		parfile:close()
 	end
 	
 	-- load one gather into mem
-	infile:seek(k)
-	raw = infile:readFloat(ns*ng)
+	if k == 1 then
+		raw = infile:readFloat(ns*ng)
+	else
+		infile:seek(k*ns*4 + 1)
+		raw = infile:readFloat(ns*ng)
+	end
 
 	-- build 2D tensor
 	for j = 1, ng do
         	for i = 1, ns do
                 	dat[i][j] = raw[i + (j-1)*ns]
 	        end
-	end
+	end	
+
+	-- condition data
+--[[	dat:add(-dat:mean())
+	dat:div(dat:std()) ]]--
 
 	-- perform covariance PCA
 	e, v = unsup.pcacov(dat)
@@ -48,7 +63,12 @@ for k = 1, ntr, ng do
 
 end
 
+-- timing
+print('total elapsed time = ', timer:time().real)
+parfile = io.open('/home/ubuntu/bbimgath/parfile', 'a')
+parfile:write('total elapsed time = ', timer:time().real)
+
 -- clean
 infile:close()
 outfile:close()
-
+parfile:close()
